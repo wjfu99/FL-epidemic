d1e9c171ad651e16410a8386c4d0e52580fae31d
wjfu <wjfu@hust.edu.cn>
2023-01-01 21:19:18 +0800

    try to multi-process on outer loop

--- Agent_Epi_Sim/data/beijing/location2region.py
+++ Agent_Epi_Sim/data/beijing/location2region.py
@@ -3,29 +3,32 @@ import shapefile
 from shapely import geometry, contains_xy
 from tqdm import tqdm
 from multiprocessing import Pool
+def region_map(region):
+    poi_locs = np.load('./processed_data/poi_loc.npy', allow_pickle=True).item()
+    outline = region[1]
+    rid = region[0]
+    locs = []
+    for pid, coord in poi_locs.items():
+        if contains_xy(outline, *coord):
+            locs.append(pid)
+    return locs
+    return None
 
-poi_locs = np.load('./processed_data/poi_loc.npy', allow_pickle=True).item()
 regions = shapefile.Reader('./raw_data/beijing-WGS/ST_R_CN_WGS.shp', encoding='latin1')
 # shapeRecs = r.shapeRecords()
-loc2region = {}
-
-
-def region_map(outline, poi_locs):
-    pid = poi_locs[0]
-    coord = poi_locs[1]
-    if contains_xy(outline, *coord):
-        return pid
-    else:
-        return None
-
+# regions = regions.iterShapeRecords()
+new_regions = []
 for region in tqdm(regions.iterShapeRecords(), total=len(regions)):
-    outline = geometry.shape(region.shape)
     record = region.record
-    pool = Pool(processes=230)
-    for pid, coord in poi_locs.items():
-        pool.apply_async(region_map, args=(outline, (pid, coord)))
-    pool.close()
-    pool.join()
+    outline = geometry.shape(region.shape)
+    new_regions.append([record['QH_CODE'], outline])
+loc2region = {}
+pool = Pool(processes=1)
+# for region in tqdm(regions.iterShapeRecords(), total=len(regions)):
+region2loc = tqdm(pool.imap(region_map, new_regions), total=6633)
+# pool.map(region_map, new_regions)
+pool.close()
+pool.join()
 
 
 ################ For test the unique id of the shp file ###########
