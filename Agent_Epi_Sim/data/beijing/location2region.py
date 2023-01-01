c9bd9196dccc0c737d47807f9849b1061c6d9a2e
wjfu <wjfu@hust.edu.cn>
2023-01-01 20:14:16 +0800

    Multi-Process loc2region (very slow).

--- Agent_Epi_Sim/data/beijing/location2region.py
+++ Agent_Epi_Sim/data/beijing/location2region.py
@@ -2,17 +2,30 @@ import numpy as np
 import shapefile
 from shapely import geometry, contains_xy
 from tqdm import tqdm
+from multiprocessing import Pool
 
 poi_locs = np.load('./processed_data/poi_loc.npy', allow_pickle=True).item()
 regions = shapefile.Reader('./raw_data/beijing-WGS/ST_R_CN_WGS.shp', encoding='latin1')
 # shapeRecs = r.shapeRecords()
 loc2region = {}
+
+
+def region_map(poi_locs):
+    pid = poi_locs[0]
+    coord = poi_locs[1]
+    if contains_xy(outline, *coord):
+        return pid
+    else:
+        return None
+
 for region in tqdm(regions.iterShapeRecords(), total=len(regions)):
     outline = geometry.shape(region.shape)
     record = region.record
-    for pid, coord in poi_locs.items():
-        if contains_xy(outline, *coord):
-            loc2region[pid] = record['QH_CODE']
+    pool = Pool(processes=230)
+    in_region = list(pool.imap(region_map, poi_locs.items()))
+    pool.close()
+    pool.join()
+    in_region = set(in_region)
 
 
 ################ For test the unique id of the shp file ###########
