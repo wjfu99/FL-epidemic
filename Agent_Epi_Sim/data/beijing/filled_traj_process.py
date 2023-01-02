44a77b183e8ffb22fdebfb828c82bd697d3f50c8
wjfu <wjfu@hust.edu.cn>
2023-01-02 15:44:07 +0800

    Revise filled_traj_process.py

--- Agent_Epi_Sim/data/beijing/filled_traj_process.py
+++ Agent_Epi_Sim/data/beijing/filled_traj_process.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 import datetime as db
 from datetime import datetime as da
@@ -24,17 +25,15 @@ def get_max(arr):
     return max_last[-1][0]
 
 # 填充轨迹，轨迹缺失部分用-1来替代
-def map_user_dict(start_time, end_time, user_traj):
+# No longer fill vacant trajectory points.
+def map_user_dict(start_time, end_time, first, last, user_traj):
     user_dict = {}
-    for u in tqdm(user_traj):
-        num = (end_time - start_time).total_seconds() // 1800 + 1
-        user_dict[u] = {}
-        for n in range(int(num)):
-            tim = start_time + n * db.timedelta(minutes=30)
-            user_dict[u][tim] = -1
-        for j in user_traj[u]:
-            if j <= end_time and j >= start_time:
-                user_dict[u][j] = user_traj[u][j]
+    for idx, u in enumerate(tqdm(user_traj)):
+        if first[idx] < start_time and last[idx] > end_time:
+            user_dict[u] = user_traj[u]
+            for time in list(user_dict[u].keys()):
+                if time < start_time or time > end_time:
+                    user_dict[u].pop(time)
     return user_dict
 
 def id_map(user_dict):
@@ -43,16 +42,15 @@ def id_map(user_dict):
     for i, j in enumerate(user_dict):
         all_dict[i] = user_dict[j]
         user_id[i] = j
-    np.save('./processed_data/user_id.npy', user_id)
+    np.save('./processed_data/user_id(filled).npy', user_id)
     return user_id, all_dict
 
 
-def traj_count_poi(user_traj, all_dict):
+def traj_count_poi(all_dict):
     locations = set()
-    for i in tqdm(user_traj):
-        traj = list(user_traj[i].values())
-        for j in traj:
-            locations.add(j)
+    for i in tqdm(all_dict):
+        traj = set(all_dict[i].values())
+        locations = locations | traj
     loc_list = list(locations)
     trace_array = []
     for info in all_dict:
@@ -68,39 +66,41 @@ def traj_covert(a2d):
         trace_array.append(a2d[info]['trace'])
     trace_array = np.array(trace_array)
     return trace_array
-
+# TODO: 需要处理某些用户访问的轨迹在POI信息列表里找不到的问题。
 def data_process_save(all_dict, poi_id):
     a1_d = {}
     for u in tqdm(all_dict):
         a1_d[u] = {}
         for j in all_dict[u]:
-            if all_dict[u][j] != -1:
-                a1_d[u][j] = poi_id[all_dict[u][j]]
+            assert all_dict[u][j] != -1
+            if str(all_dict[u][j]) in poi_id:
+                a1_d[u][j] = int(poi_id[str(all_dict[u][j])])
             else:
-                a1_d[u][j] = -1
+                nopoi.add(all_dict[u][j])
     a2d = {}
     for i in a1_d:
         a2d[i] = {}
         a2d[i]['trace'] = list(a1_d[i].values())
     traj_mat = traj_covert(a2d)
-    np.save('./processed_data/ori_data.npy', a2d)
-    np.save('./processed_data/traj_mat.npy', traj_mat)
+    np.save('./processed_data/ori_data(filled).npy', a2d)
+    np.save('./processed_data/traj_mat(filled).npy', traj_mat)
 
+nopoi = set()
 def main():
     # 这里导入的轨迹已经是poi的id了
     user_traj = np.load('./processed_data/filled_trajs.npy', allow_pickle=True).item()
     first, last = count_first_and_last(user_traj)
-    #找到最多人轨迹起始、结束的时间点
-    start_time = get_max(first)
-    end_time = get_max(last)
-    # 填充轨迹，轨迹缺失部分用-1来替代, 并且这里改成半小时为粒度的
-    user_dict = map_user_dict(start_time, end_time, user_traj)
+    # Manually select strat_time and end_time.
+    start_time = datetime.datetime(year=2016, month=9, day=19, hour=0)
+    end_time = datetime.datetime(year=2016, month=10, day=24, hour=0)
+    # Select trajectories in the selected time boundary.
+    user_dict = map_user_dict(start_time, end_time, first, last, user_traj)
     # user_dict还是以usrid的这里重新划分id
     user_id, all_dict = id_map(user_dict)
     # locations所有poi_id的set poi_id原始poiid对应新分的id
-    locations, trace_array, poi_id = traj_count_poi(user_traj, all_dict)
+    locations, trace_array, poi_id = traj_count_poi(all_dict)
     # 新增的poi_区域聚合之后的id
-    # poi_id = np.load("region_cluster/region_id.npy", allow_pickle=True).item()
+    poi_id = np.load("./processed_data/poi_loc2region(wgs84).npy", allow_pickle=True).item()
     data_process_save(all_dict, poi_id)
 
 
