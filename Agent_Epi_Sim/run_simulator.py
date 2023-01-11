2a549b8e26c7069e6b9e9e7e7c779775b6b499ee
wjfu <wjfu@hust.edu.cn>
2023-01-11 09:53:20 +0800

    reduce the init ratio to 0.00001, and set the sim length to 480*2.

--- Agent_Epi_Sim/run_simulator.py
+++ Agent_Epi_Sim/run_simulator.py
@@ -8,7 +8,7 @@ parser.add_argument("--beta", type=float, help="infected parameter beta", defaul
 parser.add_argument("--mu", type=float, help="infected parameter mu", default=0.071)
 parser.add_argument("--eps", type=float, help="infected parameter mu", default=1/14)
 parser.add_argument("--time_granularity", type=float, help="time granularity(hours)", default=0.5)
-parser.add_argument("--init_ratio", type=float, help="initialized infected case ratio", default=0.001)
+parser.add_argument("--init_ratio", type=float, help="initialized infected case ratio", default=0.0001)
 parser.add_argument("--init_mode", type=str, help="initialized infected case ratio", default="global")
 # parser.add_argument("--beta", type=float, help="infected parameter beta", default=0.1)
 
@@ -21,7 +21,7 @@ eng.load_traj(traj)
 eng.init_epi(args.init_mode, args.init_ratio)
 # eng.get_state_count
 # eng.next()
-eng.next(step_num=480*4)
+eng.next(step_num=480*2)
 print(eng.get_state_count)
 
 joblib.dump(eng.get_usr_states, "label")
