d79c1c136408d68905cff4fb7d32fc953edc0277
wjfu <wjfu@hust.edu.cn>
2023-01-23 21:58:54 +0800

    revise the wrong loc index in clients_dp.py to hyperedge index.

--- modules/utils/clients_dp.py
+++ modules/utils/clients_dp.py
@@ -13,14 +13,19 @@ class Dp_Agg(nn.Module):
         self.clip = clip
 
     def forward(self, loc_emb, fake_loc, real_loc):
-        # TODO: optimization can be considered here, simu the noise add to a location.
         # sigmod = self.C * math.sqrt(2 * math.log(1.25/self.delt), math.e) / self.eps
         # fake_loc = self.fake_loc
         # true_loc = self.true_loc
         # loc num counter
-        loc_cnt = [list(real) + list(fake) for real, fake in zip(real_loc.values(), fake_loc.values())]
-        loc_cnt = list(itertools.chain.from_iterable(loc_cnt))
-        loc_cnt = dict(Counter(loc_cnt))
+        # loc_cnt = [list(real) + list(fake) for real, fake in zip(real_loc.values(), fake_loc.values())]
+        # loc_cnt = list(itertools.chain.from_iterable(loc_cnt))
+        # loc_cnt = dict(Counter(loc_cnt))
+        edge_cnt = []
+        for real in real_loc.values():
+            edge_cnt.extend(list(real))
+        for fake in fake_loc.values():
+            edge_cnt.extend(list(fake))
+        edge_cnt = dict(Counter(edge_cnt))
         sigmod = self.clip * math.sqrt(2 * math.log(1.25 / self.delt, math.e)) / self.eps
         loc_noise = np.zeros(loc_emb.shape)
         for usr in real_loc:
@@ -28,10 +33,10 @@ class Dp_Agg(nn.Module):
             noise = np.random.normal(loc=0, scale=sigmod, size=(loc_num, loc_emb.shape[1]))
             i = 0
             for loc in real_loc[usr]:
-                loc_noise[loc] += noise[i]/loc_cnt[loc] # TODO: noise need to normalize.
+                loc_noise[loc] += noise[i]/edge_cnt[loc]
                 i += 1
             for loc in fake_loc[usr]:
-                loc_noise[loc] += noise[i]/loc_cnt[loc]
+                loc_noise[loc] += noise[i]/edge_cnt[loc]
                 i += 1
         loc_emb_noise = loc_emb + torch.Tensor(loc_noise).to(loc_emb.device)
         return loc_emb_noise
