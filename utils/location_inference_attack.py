6f9d2886891902b6121ff8138951ff0a2d19ec48
wjfu <wjfu@hust.edu.cn>
2023-01-30 23:43:14 +0800

    update location_inference_attack

--- utils/location_inference_attack.py
+++ utils/location_inference_attack.py
@@ -2,15 +2,7 @@ import numpy as np
 import os
 from tqdm import tqdm
 
-generator = "agg_iid"
-data_path = '../datasets/beijing/large-filled-clustered/'
-files = os.listdir(data_path + generator)
-traj_mat = np.load(data_path + "traj_mat(filled,sample).npy")
-traj_mat = traj_mat[:, :14*48]
-fake_mats = []
-for file in files:
-    fake = np.load(data_path + generator + '/' + file)
-    fake_mats.append(fake)
+
 
 def global_tf_mat(traj_mat):
     trajs = traj_mat
@@ -28,29 +20,42 @@ def global_tf_mat(traj_mat):
     tf_mat = np.nan_to_num(tf_mat, nan=1 / loc_num)
     return tf_mat
 
+
+generator = "uni_iid"
+data_path = '../datasets/beijing/large-filled-clustered/'
+files = os.listdir(data_path + generator)
+traj_mat = np.load(data_path + "traj_mat(filled,sample).npy")
+traj_mat = traj_mat[:, :14*48]
+fake_mats = []
+for file in files:
+    fake = np.load(data_path + generator + '/' + file)
+    fake_mats.append(fake)
+
 # tf_mat = global_tf_mat(traj_mat)
 # np.save(data_path + "tf_mat.npy", tf_mat)
+
 tf_mat = np.load(data_path + "tf_mat.npy")
-fakes_num = 10
+fakes_num = 5
+usr_num = 10000
 
 wrong_cnt = 0
-for uid in range(100):
-    real = traj_mat[uid][0:2]
-    fakes = [fake_mat[uid][0:2] for fake_mat in fake_mats]
+for uid in range(usr_num):
+    real = traj_mat[uid][0:3]
+    fakes = [fake_mat[uid][0:3] for fake_mat in fake_mats]
     fakes.insert(0, real)
     all = fakes
     prob_max = 0
     flag = False
     for i in range(fakes_num+1):
         for j in range(fakes_num+1):
-            prob = tf_mat[all[i][0], all[j][1]]
+            prob = tf_mat[all[i][0], all[j][1]] * tf_mat[all[i][1], all[j][2]]
             if prob > prob_max:
                 prob_max = prob
-                if i != 0 or j !=0:
+                if i != 0 or j != 0:
                     flag = True
                     wrong_cnt += 1
                     break
         if flag:
             break
 
-privacy = wrong_cnt / 100
+privacy = wrong_cnt / usr_num
