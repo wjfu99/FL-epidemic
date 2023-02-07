006f5192502534f69b048d6323b48ce69a9cef7a
wjfu <wjfu@hust.edu.cn>
2023-02-07 22:18:12 +0800

    remove all baselines.

--- baselines/region_block.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from tqdm import tqdm
-
-
-data_path = '../datasets/beijing/large-filled-clustered/'
-trace_array = np.load(data_path + "traj_mat(filled,sample).npy")
-trace_array = trace_array[:, 14:48]
-pop_num = trace_array.shape[0]
-label = np.load(data_path + 'label.npy')
