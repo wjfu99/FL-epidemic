4dba7e1314c730122942dcb6a3e56764f7db7491
wjfu <wjfu@hust.edu.cn>
2023-02-07 22:17:13 +0800

    remove the simulator.py

--- Agent_Epi_Sim/data/beijing/statistic_analyse/SIR_counter.py
+++
@@ -1,9 +0,0 @@
-import pickle as pkl
-import numpy as np
-from tqdm import tqdm
-
-per_data = np.load('../processed_data/per_data_omicron.npy', allow_pickle=True).item()
-
-label = [per_data[0][x]['state'] for x in per_data[0]]
-label = np.array(label).reshape(-1, 1)
-a = np.where(label==2)
